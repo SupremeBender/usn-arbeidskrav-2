4af63d08ba4ee35a0e10c8b88a550009193b0c05
SupremeBender <51387796+SupremeBender@users.noreply.github.com>
2025-11-14 20:38:27 +0100

    PEP8 formatting

--- 05_areal_omkrets.py
+++ 05_areal_omkrets.py
@@ -1,17 +1,21 @@
 import math
 
-def omkr_areal(a, b): # input diameter og høyde
-    r = a / 2 # Radius er halve diameter
+
+def omkr_areal(a, b):  # input diameter og høyde
+    r = a / 2  # Radius er halve diameter
     omkrets_halvsirkel = math.pi * r
-    areal_halvsirkel = math.pi * r**2 / 2 
+    areal_halvsirkel = math.pi * r**2 / 2
     omkrets_trekant = b + math.hypot(b, a)
     areal_trekant = a * b / 2
     omkrets_total = omkrets_halvsirkel + omkrets_trekant
     areal_total = areal_halvsirkel + areal_trekant
     return omkrets_total, areal_total
-    
+
+
 a = float(input("Skriv inn diameteren til halvsirkelen: "))
 b = float(input("Skriv inn høyden til trekanten: "))
-omkrets, areal = omkr_areal(a, b) # Kjører funksjonen fra tidligere
+
+omkrets, areal = omkr_areal(a, b)  # Kjører funksjonen fra tidligere
+
 print(f"Omkretsen av figuren: {omkrets}")
 print(f"Arealet av figuren: {areal}")
--- 06_plot.py
+++ 06_plot.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-10, 10, 200) # Generer array x med punkter
+x = np.linspace(-10, 10, 200)  # Generer array x med punkter
 
-y = -x**2 - 5 # Regn ut y basert på formelen
+y = -(x**2) - 5  # Regn ut y basert på formelen
 
-plt.plot(x, y) # Tegn grafen
-plt.show() # Vis grafen
+plt.plot(x, y)  # Tegn grafen
+plt.show()  # Vis grafen
